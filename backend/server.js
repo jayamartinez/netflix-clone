ef79b6fa0652740bf7f10c0008ca11a5dae66816
jay-desktop <jayamartinez@gmail.com>
2024-10-24 23:09:48 -0400

    Getting ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,14 @@ import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 import { protectRoute } from './middleware/protectRoute.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 
 const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json()) // allows parse of req.body
 app.use(cookieParser());
@@ -21,6 +23,15 @@ app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/movie", protectRoute, movieRoutes)
 app.use("/api/v1/tv", protectRoute, tvRoutes)
 app.use("/api/v1/search", protectRoute, searchRoutes)
+
+if (ENV_VARS.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get("*", (req, res) => {
+        res.sendFile9(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    })
+}
+
 
 
 app.listen(PORT, () => {
